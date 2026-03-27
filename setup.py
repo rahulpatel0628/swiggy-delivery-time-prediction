ddf9b0876cd0bd924591e4b61d7e084a805caf0a
Patel Rahul <rahulpatel16092005@gmail.com>
2026-03-27 18:45:08 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a mlops project to predict the delivery time in min',
+    author='rahul patel',
+    license='',
+)
